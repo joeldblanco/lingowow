5c07e82a683581a933a9c780f9231838e3ef9221
Joel Blanco <joeld.blanco@gmail.com>
2022-09-06 06:17:13 +0000

    Before pulling 06/09/2022

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('profile_photo_path')->default('profile-photos/default_pp.jpg');
+            $table->string('profile_photo_path')->default('profile-photos/default_pp.jpg');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
