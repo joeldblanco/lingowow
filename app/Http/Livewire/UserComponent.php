b96552366decca154cfba489174e51388e224b26
Joel Blanco <joeld.blanco@gmail.com>
2022-09-13 12:05:20 -0500

    Sending messages and editing users from admin.users dashboard function added.

--- app/Http/Livewire/UserComponent.php
+++ app/Http/Livewire/UserComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Enrolment;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class UserComponent extends Component
@@ -15,6 +16,43 @@ class UserComponent extends Component
     {
         $this->user = $user;
         $this->role = $role;
+    }
+
+    public function editUser($id)
+    {
+        $this->current_user = User::find($id);
+
+        $this->username = $this->current_user->username;
+        $this->password = "";
+        $this->password_confirm = "";
+        $this->first_name = $this->current_user->first_name;
+        $this->last_name = $this->current_user->last_name;
+        $this->email = $this->current_user->email;
+        $this->user_role = $this->current_user->roles[0]->id;
+    }
+
+    public function updateUser()
+    {
+        $user = User::withTrashed()->where('username', $this->username)->first();
+        if ($user != null && $user->count() > 0 && $user->trashed()) {
+            $user->restore();
+        }
+        $user->username = $this->username;
+        if (($this->password != "" && $this->password_confirm != "") && ($this->password != null && $this->password_confirm != null) && ($this->password == $this->password_confirm)) {
+            $user->password = Hash::make($this->password);
+        }
+        $user->first_name = $this->first_name;
+        $user->last_name = $this->last_name;
+        $user->email = $this->email;
+        $user->removeRole($user->roles[0]->id);
+        $user->assignRole($this->user_role);
+        $user->save();
+    }
+
+    public function deleteUser()
+    {
+        $user = User::withTrashed()->where('username', $this->username)->first();
+        $user->delete();
     }
 
     public function show()
