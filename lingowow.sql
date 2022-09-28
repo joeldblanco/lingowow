887d6489a4b7fa4bc0f586ef37157c35e562edf4
Joel Blanco <joeld.blanco@gmail.com>
2022-09-28 09:56:11 -0500

    Lingowow.sql file updated

--- lingowow.sql
+++ lingowow.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.4.18-MariaDB : Database - theuacademy
+MySQL - 10.4.18-MariaDB : Database - lingowow
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.4.18-MariaDB : Database - theuacademy
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`theuacademy` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lingowow` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `theuacademy`;
+USE `lingowow`;
 
 /*Data for the table `activities` */
 
