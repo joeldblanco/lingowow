34dd114a77769a1591f8793c41aac8a7c16f3f3c
Joel Blanco <joeld.blanco@gmail.com>
2022-09-30 11:22:50 -0500

    Registration view, some changes in log in and registration views.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,31 +17,34 @@
 
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex mt-4 justify-between">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+            </div>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+            <div class="flex flex-col items-center justify-end my-4">
+                <x-jet-button class="mx-1 w-full mt-2 mb-4 py-3">
+                    <p class="w-full font-bold">{{ __('Log in') }}</p>
                 </x-jet-button>
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Don't have an account?</a>
             </div>
         </form>
     </x-jet-authentication-card>
